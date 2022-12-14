c5aaa0be45ad051fcc0519658119e9cbddbdaf6d
Prem789538 <gprem062@gmail.com>
2022-12-14 15:20:44 +0530

    login done

--- MyBookShelf/Accounts/forms.py
+++ MyBookShelf/Accounts/forms.py
@@ -2,17 +2,18 @@ from django import forms
 from django.core.exceptions import ValidationError
 from .models import User
 
-def email_validator_sigup(email_form):
+def email_validator_signup(email_form):
     user = User.objects.filter(email=email_form)
     if user:
-        raise ValidationError(f'Email already exists!')
+        raise ValidationError('Email already exists!')
     else:
         return email_form
+
 
 class SignUpForm(forms.Form):
     FirstName = forms.CharField(max_length=50,required=True)
     LastName = forms.CharField(max_length=50,required=True)
-    Email = forms.EmailField(required=True,validators=[email_validator_sigup])
+    Email = forms.EmailField(required=True,validators=[email_validator_signup])
 
     Password = forms.CharField(widget=forms.PasswordInput(),min_length=5,max_length=15)
     PasswordConfirm = forms.CharField(widget=forms.PasswordInput(),min_length=5,max_length=15)
--- MyBookShelf/Accounts/views.py
+++ MyBookShelf/Accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.contrib import messages
-from .forms import SignUpForm
+from django.contrib.auth.hashers import make_password,check_password
+from .forms import SignUpForm,LoginForm
 from .models import User
 def home(req):
     return render(req,'accounts/home.html')
@@ -17,6 +18,7 @@ def register(req):
             firstname = data['FirstName']
             lastname = data['LastName']
             email = data['Email']
+            #passwd = make_password(data['Password'])
             passwd = data['Password']
 
             user = User.objects.create(full_name=firstname+" "+lastname,email=email,password=passwd)
@@ -34,4 +36,26 @@ def register(req):
 
 
 def login(req):
-    return render(req,'accounts/login.html')
+    if req.method=="POST":
+        form = LoginForm(req.POST)
+        if form.is_valid():
+            email = form.cleaned_data['Email']
+            passwd = form.cleaned_data['Password']
+
+            user = User.objects.filter(email=email).first()
+            if user and user.password == passwd:
+                messages.success(req,'Login Successful')
+
+                return redirect(reverse('account_home'))
+            else:
+                messages.warning(req,'Login failed!')
+
+
+                
+
+
+
+    else:
+        form = LoginForm()
+    
+    return render(req,'accounts/login.html',{'form':form})
